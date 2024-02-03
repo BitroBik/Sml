e4e3b821a7bb18ca3660f07319c0518292b7bdf5
BitroBik <158396747+BitroBik@users.noreply.github.com>
2024-02-03 20:03:55 +0800

    Update PyXml.py
    Added nesting Tag

--- Source/PyXml.py
+++ Source/PyXml.py
@@ -1,28 +1,29 @@
+def process(content, indent=''):
+    process_data = ''
+    
+    for c in content:
+        if c.type == 'Tag':
+            process_data += f'\n{indent}{c.name}'
+            process_data += process(c.content, f'{indent}  ')
+        else:
+            process_data += f'\n{indent}{c.text}'
+    
+    return process_data
+
 class Tag:
     def __init__(self, name, content=[]):
         self.name = name
         self.content = content
         self.type = 'Tag'
 
-class Msg:
+class Text:
     def __init__(self, text):
         self.text = text
-        self.type = 'Msg'
+        self.type = 'Text'
 
 class Code:
     def __init__(self, content=[]):
         self.content = content
         
-        try:
-            self.tag = {}
-            for cont in self.content:
-                if cont.type == 'Tag':
-                    self.tag.update({cont.name: cont.content})
-        except AttributeError as e:
-            print(f'Unexpected Error: {e}')
-
     def Run(self):
-        for key in self.tag:
-            print(key)
-            for msg in self.tag[key]:
-                print(msg.text)
+        print(process(self.content).lstrip())
