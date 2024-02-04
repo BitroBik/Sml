7541515e3b33b285fbc833b2e77f5eb63a6aa9e4
BitroBik <158396747+BitroBik@users.noreply.github.com>
2024-02-04 09:28:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyxml',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     setup_requires=[
         'pip>=3.11',
