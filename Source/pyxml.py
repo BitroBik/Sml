ef5635da68b54e13fde43614e25af90ca94b6e40
BitroBik <158396747+BitroBik@users.noreply.github.com>
2024-02-04 12:00:15 +0800

    Update pyxml.py

--- Source/pyxml.py
+++ Source/pyxml.py
@@ -1,16 +1,19 @@
-def process(content, indent=''):
+def process(content, up, indent=''):
     process_data = ''
 
     for c in content:
         if c.type == 'Tag':
             process_data += f'\n{indent}{c.name}'
-            process_data += process(c.content, f'{indent}  ')
+            process_data += process(c.content, up, f'{indent}{up}')
         elif c.type == 'Entry':
             process_data += f'\n{indent}{c.name}{input(c.text)}'
             print('')
         elif c.type == 'Text':
             process_data += f'\n{indent}{c.text}'
-
+        elif c.type == 'Code':
+            process_data = 'Cannot Nest Code only Tag'
+            break
+            
     return process_data
 
 class Tag:
@@ -31,8 +34,25 @@ class Text:
         self.type = 'Text'
 
 class Code:
-    def __init__(self, content=[]):
+    def __init__(self, name, content, up='  '):
+        self.name = name
+        self.up = up
         self.content = content
+        self.will_run = True
+        self.type = 'Code'
+        
+        if isinstance(self.up, str):
+            pass
+        else:
+            self.will_run = False
+            print(f'self.up must be a string: Code("{self.name}", [], "  ")')
+        
+        if isinstance(self.content, list):
+            pass
+        else:
+            self.will_run = False
+            print(f'self.content must be a list: [{content.name}]')
         
     def Run(self):
-        print(process(self.content).lstrip())
+        if self.will_run:
+            print(process(self.content, self.up).lstrip())
