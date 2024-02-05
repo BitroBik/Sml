b5ab496aab20537daa74eebe29a7a198dfc0a6bc
BitroBik <158396747+BitroBik@users.noreply.github.com>
2024-02-05 15:44:48 +0800

    Update pyxml.py

--- Source/pyxml.py
+++ Source/pyxml.py
@@ -3,32 +3,70 @@ def process(content, up, indent=''):
 
     for c in content:
         if c.type == 'Tag':
-            if c.attributes == '':
+            if c.attribute == {}:
                 process_data += f'\n{indent}{c.name}'
                 process_data += process(c.content, up, f'{indent}{up}')
             else:
-                process_data += f'\n{indent}{c.name}[{c.attributes}]'
-                process_data += process(c.content, up, f'{indent}{up}')
+                for key in c.attribute:
+                    process_data += f'\n{indent}{c.name}[{key}="{c.attribute[key]}"]'
+                    process_data += process(c.content, up, f'{indent}{up}')
         elif c.type == 'Entry':
             process_data += f'\n{indent}{c.name}{input(c.text)}'
             print('')
         elif c.type == 'Text':
-            if c.attributes == '':
+            if c.attribute == {}:
                 process_data += f'\n{indent}{c.text}'
             else:
-                process_data += f'\n{indent}{c.text}[{c.attributes}]'
+                for key in c.attribute:
+                    process_data += f'\n{indent}{c.text}[{key}="{c.attribute[key]}"]'
         elif c.type == 'Code':
             process_data = 'Cannot Nest Code only Tag'
             break
             
     return process_data
 
+def Xmlified(content, indent=''):
+    process_data = ''
+
+    for c in content:
+        if c.type == 'Tag':
+            if c.attribute == {}:
+                process_data += f'\n{indent}<{c.name}>'
+                process_data += Xmlified(c.content, f'{indent}  ')
+                process_data += f'\n{indent}</{c.name}>'
+            else:
+                for key in c.attribute:
+                    process_data += f'\n{indent}<{c.name} {key}="{c.attribute[key]}">'
+                    process_data += Xmlified(c.content, f'{indent}  ')
+                    process_data += f'\n{indent}</{c.name}>'
+                    break
+        elif c.type == 'Entry':
+            process_data += f'\n{indent}<{c.name}>{input(c.text)}</{c.name}>'
+            print('')
+        elif c.type == 'Text':
+            if c.attribute == {}:
+                process_data += f'\n{indent}<{c.name}>{c.text}</{c.name}>'
+            else:
+                for key in c.attribute:
+                    process_data += f'\n{indent}<{c.name} {key}="{c.attribute[key]}>{c.text}</{c.name}>"'
+    return process_data
+
 class Tag:
-    def __init__(self, name, content=[], attributes=''):
+    def __init__(self, name, content=[], attribute={}):
         self.name = name
         self.content = content
-        self.attributes = attributes
+        self.attribute = attribute
         self.type = 'Tag'
+        
+        if isinstance(self.content, list):
+            pass
+        else:
+            self.content = [content]
+            print(f'self.content must be a list: {self.name}')
+        if isinstance(self.attribute, dict):
+            pass
+        else:
+            self.attribute = {'Error':'self.attribute must be on a dictionary'}
 
 class Entry:
     def __init__(self, name, text):
@@ -37,11 +75,17 @@ class Entry:
         self.type = 'Entry'
     
 class Text:
-    def __init__(self, text, attributes=''):
+    def __init__(self, name, text, attribute={}):
+        self.name = name
         self.text = text
-        self.attributes = attributes
+        self.attribute = attribute
         self.type = 'Text'
-
+        
+        if isinstance(self.attribute, dict):
+            pass
+        else:
+            self.attribute = {'Error':'self.attribute must be on a dictionary'}
+                
 class Code:
     def __init__(self, name, content, up='  '):
         self.name = name
@@ -65,3 +109,7 @@ class Code:
     def Run(self):
         if self.will_run:
             print(process(self.content, self.up).lstrip())
+    
+    def Turn_Xml(self):
+        if self.will_run:
+            print(Xmlified(self.content).lstrip())
